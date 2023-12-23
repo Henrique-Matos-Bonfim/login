9832f572bf869dbeb5aaabee6fbfcf1acb8c4a95
Henrique <matosh38@gmail.com>
2023-12-23 20:45:54 -0300

    criando a interface gráfica

---
+++ screen.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+
+# Cores
+
+fundo = '#3b3b3b'
+
+
+
+#setando a janela
+janela = Tk()
+janela.title('Login')
+janela.geometry('480x360')
+janela.configure(bg=fundo)
+
+
+
+janela.mainloop()
